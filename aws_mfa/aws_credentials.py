12ec945d991f10ba4410411c941ec0271b4c882b
Ryan Faircloth <5642043+ryanrf@users.noreply.github.com>
2023-01-16 17:49:50 -0500

    fixed a messy merge commit

--- aws_mfa/aws_credentials.py
+++ aws_mfa/aws_credentials.py
@@ -3,7 +3,6 @@ from configparser import ConfigParser
 from datetime import datetime
 from os import environ
 from pathlib import Path, PurePath
-from sys import exit
 from typing import Optional
 
 import boto3
@@ -21,8 +20,6 @@ from aws_mfa.exceptions import (
     NoMfaDeviceFound,
 )
 
-from os import environ
-
 
 class AwsCredentials:
     def _get_auth_method(self, session):
@@ -34,6 +31,7 @@ class AwsCredentials:
     def _get_client_for_profile(self, profile: str, svc: str) -> boto3.client:
         session = boto3.Session(profile_name=profile)
         self.aws_auth_method = self._get_auth_method(session)
+        self.logger.debug("AWS authentication method: %s" % self.aws_auth_method)
         self.logger.debug("Using profile: %s with service %s" % (profile, svc))
         return session.client(svc)
 
@@ -62,20 +60,9 @@ class AwsCredentials:
         self.aws_credentials_config = self.load_creds_file()
         self.username = self.iam_client.get_user()["User"]["UserName"]
 
-    def _get_auth_method(self, session):
-        if session.get_credentials():
-            return session.get_credentials().method
-        else:
-            raise AwsCredentialsNotFound("No AWS credentials were found")
-
-    def _get_client_for_profile(self, profile: str, svc: str) -> boto3.client:
-        session = boto3.Session(profile_name=profile)
-        self.aws_auth_method = self._get_auth_method(session)
-        self.logger.debug("AWS authentication method: %s" % self.aws_auth_method)
-        self.logger.debug("Using profile: %s with service %s" % (profile, svc))
-        return session.client(svc)
-
-    def _check_mfa_enabled(self, credentials_config: ConfigParser = ConfigParser()) -> bool:
+    def _check_mfa_enabled(
+        self, credentials_config: ConfigParser = ConfigParser()
+    ) -> bool:
         if not credentials_config:
             credentials_config = self.aws_credentials_config
         return (
